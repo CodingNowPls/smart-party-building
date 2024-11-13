216460ec35f6c407ac5bdc8fed06d6d82c6797d0
gao <1937982680@qq.com>
2024-11-13 11:49:58 +0800

    init

--- admin/src/main/java/com/ruoyi/web/controller/tool/DeptInfo.java
+++ admin/src/main/java/com/ruoyi/web/controller/tool/DeptInfo.java
@@ -11,35 +11,4 @@ public class DeptInfo {
     private Long parentId;
     private List<DeptInfo> children;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<DeptInfo> getChildren() {
-        return children;
-    }
-
-    public void setChildren(List<DeptInfo> children) {
-        this.children = children;
-    }
 }
---
+++ portal/src/main/java/com/ruoyi/party/building/portal/common/controller/PortalApiController.java
@@ -0,0 +1,14 @@
+package com.ruoyi.party.building.portal.common.controller;
+
+
+import com.ruoyi.common.core.controller.BaseController;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("PC端接口")
+@RestController
+@RequestMapping("/api")
+public class PortalApiController extends BaseController {
+
+}
